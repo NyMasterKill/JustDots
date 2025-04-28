7a139654b4012ea706931b45f0efe94e430e5700
Кирилл <190339274+NyMasterKill@users.noreply.github.com>
2025-04-28 17:14:50 +0500

    Delete src directory

--- src/skills/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
-from src.database import Base
-
-class Skill(Base):
-    __tablename__ = "skills"
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False, unique=True)
-    users = relationship("UserSkill", back_populates="skill")
